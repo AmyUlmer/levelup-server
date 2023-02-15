80debe5d870b910bc79122e2223aab63393c83d9
Amy Ulmer <aulmer703@gmail.com>
2023-02-15 15:24:19 -0600

    updating fixtures

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -5,8 +5,6 @@ class Game(models.Model):
     game_type = models.ForeignKey("GameType", on_delete=models.CASCADE)
     gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
-    estimated_time = models.DecimalField(max_digits=2, decimal_places=1)
     min_age = models.IntegerField()
-    max_age = models.IntegerField()
     min_players = models.IntegerField()
     max_players = models.IntegerField()
